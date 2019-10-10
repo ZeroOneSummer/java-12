8ca456ee90cb3e7c37693420162ca966f49d2326
v_pijiang <aaa>
2019-10-10 14:28:21 +0800

    concurrent并发编程包

--- src/main/java/com/dimai/project/demo/test/Demo.java
+++ src/main/java/com/dimai/project/demo/test/Demo.java
@@ -7,27 +7,28 @@ import java.util.function.Function;
 
 /**
  * Created by pijiang on 2019/8/28.
+ * 适用于java12
  */
 public class Demo {
 
     private static void switchDemo(Color color){
-        var a = "yes";
-        var b = 400;
-        var c = new Date();
-        switch (color) {
-            case RED,YELLOW,GLAY -> System.out.println(a);
-            case GREEN -> System.out.println(b);
-            case PINK -> System.out.println(c);
-        }
+//        var a = "yes";
+//        var b = 400;
+//        var c = new Date();
+//        switch (color) {
+//            case RED,YELLOW,GLAY -> System.out.println(a);
+//            case GREEN -> System.out.println(b);
+//            case PINK -> System.out.println(c);
+//        }
     }
 
     private static void strTransDemo(String str){
-        System.out.println(str.transform(new Function<String, String>() {
-            @Override
-            public String apply(String s) {
-                return StringUtils.capitalize(s);
-            }
-        }));
+//        System.out.println(str.transform(new Function<String, String>() {
+//            @Override
+//            public String apply(String s) {
+//                return StringUtils.capitalize(s);
+//            }
+//        }));
     }
 
     public static void main(String[] args) {
--- src/main/java/com/dimai/project/demo/thread_sysn/Demo1.java
+++ src/main/java/com/dimai/project/demo/thread_sysn/Demo1.java
@@ -1,12 +1,18 @@
 package com.dimai.project.demo.thread_sysn;
 
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by pijiang on 2019/9/12.
  */
 class Test extends Thread {
     private int count;
 
-    void setCount(int count){
+    void setCount(int count) {
         this.count = count;
     }
 
@@ -28,8 +34,24 @@ class Test extends Thread {
 }
 
 
-public class Demo1 {
-    public static void main(String[] args) throws Exception{
+class Demo1 {
+    final Lock lock = new ReentrantLock();
+
+    //Lock配合Condition使用
+    void conditionDemo() throws InterruptedException {
+        final Condition notFull = lock.newCondition();
+        final Condition notEmpty = lock.newCondition();
+
+        lock.lock();
+        try {
+            notFull.await();
+        } finally {
+            lock.unlock();
+        }
+        notEmpty.signal();
+    }
+
+    public static void main(String[] args) throws Exception {
         int num = 10;
         Test test = new Test();
         test.setCount(num);
@@ -49,5 +71,63 @@ public class Demo1 {
             System.out.println(Thread.currentThread().getName() + ": " + num);
         }
         System.out.println("main结束");
+    }
+}
+
+
+class Demo2 {
+
+    //利用计数器处理批量任务后再执行后续操作
+    public static void main(String[] args) throws Exception {
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "执行了");
+            countDownLatch.countDown();
+        }).start();
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "执行了");
+            countDownLatch.countDown();
+        }).start();
+
+        countDownLatch.await();
+        System.out.println("子线程全部执行了，开始执行主线程。。。");
+    }
+}
+
+class Demo3{
+    //原子自增
+    public static void main(String[] args) {
+        AtomicInteger atomicInteger = new AtomicInteger();
+        System.out.println(atomicInteger.incrementAndGet());
+        System.out.println(atomicInteger.incrementAndGet());
+    }
+}
+
+class Demo4{
+    //线程池
+    public static void main(String[] args) {
+        ExecutorService scheduledExecutor = Executors.newScheduledThreadPool(3);
+        for (int i = 0; i < 3; i++)
+        scheduledExecutor.execute(new Thread(() -> {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "执行完毕");
+        }));
+        scheduledExecutor.shutdown();
     }
 }
